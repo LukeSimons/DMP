314715fd68e5a7157d75eb922f5f8f3f722dacea
Luke Simons <ls5115@login-3-internal.cx1.hpc.ic.ac.uk>
2017-10-09 14:47:57 +0100

    Altered Build and Job Script: Altered the build and job scripts to correctly build (with the compiler flag -qopenmp) and to set the OMPNUMTHREADS variable to 96 in this case.

---
+++ build_CX1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+module load intel-suite
+icc -O3 -std=c++14 -qopenmp -xSSE4.2 -axAVX,CORE-AVX-I,CORE-AVX2 -o DMP main.cpp threevector.cpp Constants.cpp -I.
+
+#icc -std=c++14 -xCORE-AVX-I -o DMP main.cpp threevector.cpp Constants.cpp -I.
