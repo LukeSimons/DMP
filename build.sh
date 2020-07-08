884f1f3474e1779601aac2da08c3810a3991b137
Daniel Greenhouse <dmgreenhouse@outlook.com>
2020-07-08 10:34:31 +0100

    Added Field_Point class to compile line in bash scripts, compiles with no known bugs.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-g++ -std=c++14 -fopenmp -o3 -Wall main.cpp Constants.cpp Functions.cpp threevector.cpp rand_mwts.cpp -I. -o main
-#g++ -std=c++14 -o3 -Wall Tests/tmp.cpp threevector.cpp Constants.cpp rand_mwts.cpp -I. -o tmp
-g++ -std=c++14 -o3 -Wall Tests/Tests.cpp threevector.cpp Constants.cpp rand_mwts.cpp -I. -I/Tests/ -o test
-#g++ -std=c++14 -fopenmp -o3 -Wall main.cpp Constants.cpp Functions.cpp threevector.cpp rand_mwts.cpp -I. -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include/ -L/usr/lib/x86_64-linux-gnu/hdf5/serial/ -lhdf5 -lhdf5_hl -lhdf5_cpp -lhdf5_hl_cpp -o main
+g++ -std=c++14 -fopenmp -o3 -Wall main.cpp Constants.cpp Functions.cpp threevector.cpp rand_mwts.cpp Field_Point.cpp -I. -o main
+#g++ -std=c++14 -o3 -Wall Tests/tmp.cpp threevector.cpp Constants.cpp rand_mwts.cpp Field_Point.cpp -I. -o tmp
+g++ -std=c++14 -o3 -Wall Tests/Tests.cpp threevector.cpp Constants.cpp rand_mwts.cpp Field_Point.cpp -I. -I/Tests/ -o test
+#g++ -std=c++14 -fopenmp -o3 -Wall main.cpp Constants.cpp Functions.cpp threevector.cpp rand_mwts.cpp Field_Point.cpp -I. -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include/ -L/usr/lib/x86_64-linux-gnu/hdf5/serial/ -lhdf5 -lhdf5_hl -lhdf5_cpp -lhdf5_hl_cpp -o main
